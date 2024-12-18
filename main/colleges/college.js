27e67da5caa9cbf78d264ad2a08aee0a9358fa76
DevaramRoshan <roshanhambad735@gmail.com>
2024-12-18 12:19:06 +0530

    added functionaloty in colleges files

--- main/colleges/college.js
+++ main/colleges/college.js
@@ -1,10 +1,13 @@
 const tableBody = document.querySelector(".tableBody");
 const table = document.getElementById("table");
+const updateInputList = document.getElementsByClassName("updateInputs");
+const updateForm = document.getElementById("updateForm");
+const search = document.getElementById("search");
+const addCollegeForm = document.getElementById("addCollegeForm");
 
 async function showCollegeData() {
     const result = await fetch("https://campuspulseserver.onrender.com/colleges");
     const data = await result.json();
-    console.log(data);
     loadCollegesTable(data);
 }
 
@@ -12,11 +15,9 @@ function loadCollegesTable(arr) {
     const tbody = document.createElement("tbody");
     arr.forEach((val) => {
         const values = Object.values(val);
-        console.log(values);
         const tr = document.createElement("tr");
         const img = document.createElement("img");
         const td = document.createElement("td");
-        console.log(val.schoolLogoUrl);
         img.setAttribute("src", `${val.collegeLogoUrl}`);
         img.style = `
         height:100px;
@@ -30,9 +31,97 @@ function loadCollegesTable(arr) {
             td.textContent = values[i];
             tr.appendChild(td);
         }
+        const tdButton = document.createElement("td");
+        const updateBtn = document.createElement("button");
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        updateBtn.textContent = "Update";
+        updateBtn.setAttribute("class", "actionBtns btn btn-primary");
+        updateBtn.setAttribute("type", "button");
+        updateBtn.setAttribute("data-bs-toggle", "modal");
+        updateBtn.setAttribute("data-bs-target", "#staticBackdrop");
+
+        deleteBtn.setAttribute("class", "actionBtns btn btn-primary");
+
+        updateBtn.addEventListener("click", (e) => {
+            showUpdateForm(val);
+        });
+        deleteBtn.addEventListener("click", (e) => {
+            deleteCollege(val);
+        });
+        tdButton.appendChild(updateBtn);
+        tdButton.appendChild(deleteBtn);
+        tr.appendChild(tdButton);
         tbody.appendChild(tr);
     });
 
     table.appendChild(tbody);
 }
+
+
+function showUpdateForm(val) {
+    updateInputList[0].value = val.collegeLogoUrl;
+    updateInputList[1].value = val.collegeName;
+    updateInputList[2].value = val.collegeAdmin;
+    updateInputList[3].value = val.email;
+    updateInputList[4].value = val.password;
+    sessionStorage.clear();
+    sessionStorage.setItem("id", val._id);
+}
+
+updateForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    e.stopPropagation();
+    const fd = new FormData(updateForm);
+    const urlencoded = new URLSearchParams(fd);
+    const id = sessionStorage.getItem("id");
+    sessionStorage.removeItem("id");
+    const result = await fetch(`https://campuspulseserver.onrender.com/colleges/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded"
+        },
+        body: urlencoded
+    });
+    const resultData = await result.json();
+    if (resultData._id == id) {
+        window.location.reload();
+    }
+    else {
+        alert("error occured while updating");
+    }
+})
+
+async function deleteCollege(val) {
+    const result = await fetch(`https://campuspulseserver.onrender.com/colleges/${val._id}`, {
+        method: "DELETE"
+    });
+    const resultData = await result.json();
+    if (resultData.message == "deleted college") {
+        window.location.reload();
+    }
+    else {
+        alert("error occured while deleting college");
+    }
+}
+
+addCollegeForm.addEventListener("submit", async (e) => {
+    const fd = new FormData(addCollegeForm);
+    const urlencoded = new URLSearchParams(fd);
+    const sendCollegeData = await fetch(`https://campuspulseserver.onrender.com/colleges`, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded"
+        },
+        body: urlencoded
+    });
+    const receivedData = await sendCollegeData.json();
+    console.log(receivedData);
+})
+
+search.addEventListener("input", async () => {
+
+})
+
 showCollegeData();
